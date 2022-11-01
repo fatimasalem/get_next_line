5fe2e3b0ed4a3a72f4652741cc50accd9db41cee
Fatima Salem <106735363+FatimaSalem@users.noreply.github.com>
2022-11-01 19:36:06 +0400

    Add files via upload

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsalem <fsalem@student.42abudhabi.ae>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/30 16:56:03 by fsalem            #+#    #+#             */
+/*   Updated: 2022/04/19 14:00:00 by fsalem           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+
+char	*ft_strjoin(char *s1, char *s2);
+int		ft_strlen(char *str);
+char	*ft_strchr(char *str, int c);
+char	*ft_save(char *save);
+char	*get_next_line(int fd);
+char	*ft_read(int fd, char *str);
+char	*ft_get_line(char *str);
+
+#endif
